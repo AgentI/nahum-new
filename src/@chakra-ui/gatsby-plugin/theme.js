9161f76a766552814255d7c7dfd2e64e1a08d096
Creotip <creotip@gmail.com>
2022-01-18 13:21:43 +0200

    drawer

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -4,6 +4,7 @@ import { extendTheme } from '@chakra-ui/react'
 // 2. Extend the theme to include custom colors, fonts, etc
 
 export default extendTheme({
+  direction: 'rtl',
   fonts: {
     heading:
       '"IBM Plex Sans Hebrew",-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react'
 import Logo from './logo'
-import { Box, Button, Flex, List, ListItem, IconButton } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  List,
+  ListItem,
+  IconButton,
+  useDisclosure,
+  DrawerBody,
+  DrawerContent,
+  Input,
+  DrawerFooter,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  Drawer,
+} from '@chakra-ui/react'
 import Container from './container'
 import { BsWhatsapp } from 'react-icons/bs'
 import { HiOutlineMenuAlt3 } from 'react-icons/hi'
@@ -8,6 +24,8 @@ import { HiOutlineMenuAlt3 } from 'react-icons/hi'
 import { Link } from 'gatsby'
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <Container
       as="header"
@@ -18,8 +36,20 @@ const Header = () => {
       gridTemplateColumns="1fr 1fr 1fr"
       mb={4}
     >
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
       <Box as="nav">
         <IconButton
+          onClick={onOpen}
           display={['block', 'block', 'none']}
           variant="ghost"
           aria-label="mobile menu"
